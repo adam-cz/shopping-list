9f42e641b3af3f9a898d0d056acf19713bdb4ad4
adam-cz <joura.adam@email.cz>
2021-03-04 14:16:22 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const todoModel = {
+    productName: "",
+    inCart: false,
+    icon: "",
+  };
+
+  const addTodo = () => {
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((value, pos) => pos !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list">
+      <h1>Nákupní seznam</h1>
+      <div className="input">
+        <input type="text" onChange={onChange} value={input} />
+        <button onClick={addTodo}>Přidat</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>X</button>
+            <button>Hotovo</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
